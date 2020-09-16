3555eb5e092aa85b1fe7d7a699833a42276cf7ce
harvey <me@ngvuong.com>
2020-09-16 15:01:22 +0200

    Refactor the python script

--- demo/ac3r.py
+++ demo/ac3r.py
@@ -1,102 +1,99 @@
 import os
 import sys
 from beamngpy import BeamNGpy, Scenario, Vehicle, setup_logging
-from beamngpy.sensors import Damage, Electrics, GForces
+from beamngpy.sensors import Damage
 
+class AC3R:
+	def __init__(self, bng_log, scenario):
+		self.bng_home = os.getenv('BNG_HOME')
+		self.bng_research = os.getenv('BNG_RESEARCH')
+		self.host = '127.0.0.1'
+		self.port = 64257
+		self.vehicles = []
+		self.bng_log = bng_log
+		self.scenario = scenario
+		self.level = 'smallgrid'
+		self.timeout = 20
 
-def beamng():
-	setup_logging()
-	# Get environment variables
-	BNG_HOME = os.getenv('BNG_HOME')
-	BNG_RESEARCH = os.getenv('BNG_RESEARCH')
-	host = '127.0.0.1'
-	port = 64257
-	# Instantiates a BeamNGpy instance
-	beamng = BeamNGpy(host, port, BNG_HOME, BNG_RESEARCH)
-	return beamng
+	def create_vehicle(self, vehicle_objs):
+		for v in vehicle_objs:
+			# Creates vehicle with associated id
+			vehicle = Vehicle(str(v.id))
+			damage = Damage()
+			# Attach Sensor
+			vehicle.attach_sensor('damage', damage)
+			self.vehicles.append(vehicle)
 
-def main(beamng, scenario_name):
-	# Find a scenario in the smallgrid level called 'Case4'
-	level = 'smallgrid'
-	scenario = Scenario(level, scenario_name)
-	# Update the scenario instance with its path
-	scenario.find(beamng)
-
-	bng = beamng.open(launch=True)
-	bng.hide_hud()
-	bng.set_deterministic() # Set simulator to be deterministic mode
-
-	# Load and start the scenario
-	bng.load_scenario(scenario)
+	def write_log(self, damage):
+		with open(self.bng_log, 'w') as f:
+			print(damage, file=f)
 	
-	# Gets vehicles placed in the scenario
-	vehicle_bng = 'BeamNGVehicle'
-	vehicles = create_vehicle(bng.find_objects_class(vehicle_bng))
+	def close_scenario(self, bng):
+		bng.kill_beamng()
+		sys.exit('----- Close BeamNG connection -----')
+	
+	def start_beamng(self):
+		# Instantiates a BeamNGpy instance
+		return BeamNGpy(self.host, self.port, self.bng_home, self.bng_research)
 
-	# bng.set_steps_per_second(10)
-	for vehicle in vehicles:
-		bng.connect_vehicle(vehicle)
-		assert vehicle.skt
+	def execute_scenario(self, bng):
+		scenario = Scenario(self.level, self.scenario)
+		# Update the scenario instance with its path
+		scenario.find(bng)
 
-	# Wait for 75 steps before start
-	bng.step(75, wait=True)
-	bng.start_scenario() # Start scenario
+		# Configures BeamNG client
+		bng.open(launch=True)
+		bng.hide_hud()
+		bng.set_deterministic() # Set simulator to be deterministic mode
 
-
-	for _ in range(20):
-		# Collects sensor data every 30 steps
-		bng.step(30)
-
-		accident_log = {}
-		for vehicle in vehicles:
-			sensor = bng.poll_sensors(vehicle)['damage']
-			if (sensor['damage'] != 0): # Crash detected
-				print("Crash detected!")
-				accident_log.update( { vehicle.vid: sensor['partDamage'] } )
+		# Load and start the scenario
+		bng.load_scenario(scenario)
 		
-		empty = not bool(accident_log)
-		if not empty:
-			write_log(accident_log)
-			bng.step(150, wait=True)
-			print("Within time!")
-			close_scenario(bng)
+		# Gets vehicles placed in the scenario
+		vehicle_bng = 'BeamNGVehicle'
+		self.create_vehicle(bng.find_objects_class(vehicle_bng))
 
-	# Timeout
-	print("Timed out!")
-	close_scenario(bng)
+		# bng.set_steps_per_second(10)
+		for vehicle in self.vehicles:
+			bng.connect_vehicle(vehicle)
+			assert vehicle.skt
 
-def close_scenario(beamng):
-	beamng.kill_beamng()
-	sys.exit('----- Close BeamNG connection -----')
+		# Wait for 75 steps before start
+		waiting_steps = 75
+		bng.step(waiting_steps, wait=True)
+		bng.start_scenario() # Start scenario
 
-def write_log(damage):
-	with open('accidentLog.txt', 'w') as f:
-		print(damage, file=f)
+		for _ in range(self.timeout):
+			# Collects sensor data every 30 steps
+			bng.step(30)
 
-	
+			accident_log = {}
+			for vehicle in self.vehicles:
+				sensor = bng.poll_sensors(vehicle)['damage']
+				if (sensor['damage'] != 0): # Crash detected
+					print("Crash detected!")
+					accident_log.update( { vehicle.vid: sensor['partDamage'] } )
+			
+			empty = not bool(accident_log)
+			if not empty:
+				waiting_steps = 175
+				self.write_log(accident_log)
+				bng.step(waiting_steps, wait=True)
+				print("Within time!")
+				self.close_scenario(bng)
 
-def create_vehicle(vehicle_objs):
-	vehicles = []
-	for v in vehicle_objs:
-		# Creates vehicle with associated id and attaches damage sensor to each vehicle
-		vehicle = Vehicle(str(v.id))
-		damage = Damage()
-		electrics = Electrics()
-		gForces = GForces()
-
-		# Attach Sensor
-		vehicle.attach_sensor('damage', damage)
-		# vehicle.attach_sensor('electrics', electrics)
-		# vehicle.attach_sensor('gForces', gForces)
-		vehicles.append(vehicle)
-	return vehicles
+		# Timeout
+		print("Timed out!")
+		self.close_scenario(bng)
 
 
-if __name__ == '__main__':
-	beamng = beamng()
-
-	# Creates empty log file
-	with open('accidentLog.txt', 'w') as fp: 
-		pass
-	main(beamng, sys.argv[1]) # Ex: python C:\Users\Harvey\Documents\AC3R-Demo\demo\ac3r.py Case0
-
+# Defines log file locations
+bng_log = os.path.dirname(__file__) + '\\logCases\\' + sys.argv[1] + '.txt'
+# Creates empty log file
+with open(bng_log, 'w') as fp:
+	pass
+# Executing the given scenario
+setup_logging()
+ac3r = AC3R(bng_log, sys.argv[1])
+bng = ac3r.start_beamng()
+ac3r.execute_scenario(bng)
