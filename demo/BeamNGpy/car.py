928a84943ae6850d06b6bf8f3eb39114242006f4
harvey <me@ngvuong.com>
2020-09-17 14:28:21 +0200

    Update the map to fit AC3R principles

--- demo/BeamNGpy/car.py
+++ demo/BeamNGpy/car.py
@@ -1,6 +1,22 @@
 from beamngpy import Vehicle
 
-CART_PARTS_DICT = {'Tailgate': 'tailgate', 'Wagon Unibody': 'wagon_unibody', 'Rear Bumper': 'rear_bumper', 'Front Bumper Support': 'front_bumper_support', 'Front Bumper': 'front_bumper', 'Hood': 'hood', 'Right Headlight': 'right_headlight', 'Left Headlight': 'left_headlight', 'Front Right Fender': 'front_right_fender', 'Front Left Fender': 'front_left_fender', 'Single Exhaust': 'single_exhaust', 'Front Right Door': 'front_right_door', 'Front Left Door': 'front_left_door', 'Rear Right Door': 'rear_right_door', 'Rear Left Door': 'rear_left_door'}
+CART_PARTS_DICT = {
+  'Tailgate': 'R',
+  'Wagon Unibody': 'M',
+  'Rear Bumper': 'R',
+  'Front Bumper Support': 'F',
+  'Front Bumper': 'F',
+  'Hood': 'F',
+  'Right Headlight': 'FR',
+  'Left Headlight': 'FL',
+  'Front Right Fender': 'FR',
+  'Front Left Fender': 'FL',
+  'Single Exhaust': 'R',
+  'Front Right Door': 'MR',
+  'Front Left Door': 'ML',
+  'Rear Right Door': 'MR',
+  'Rear Left Door': 'ML'
+}
 
 class Car:
 	def __init__(self, id, vehicle):
