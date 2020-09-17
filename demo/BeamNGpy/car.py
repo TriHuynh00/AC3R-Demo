88b40c67329f4a226ba3226a69f8b753b710881c
harvey <me@ngvuong.com>
2020-09-17 13:03:06 +0200

    Implements writing damageLog function to use with AC3R mechanism

--- demo/BeamNGpy/car.py
+++ demo/BeamNGpy/car.py
@@ -1,14 +1,30 @@
-import os
-import sys
 from beamngpy import Vehicle
 
 CART_PARTS_DICT = {'Tailgate': 'tailgate', 'Wagon Unibody': 'wagon_unibody', 'Rear Bumper': 'rear_bumper', 'Front Bumper Support': 'front_bumper_support', 'Front Bumper': 'front_bumper', 'Hood': 'hood', 'Right Headlight': 'right_headlight', 'Left Headlight': 'left_headlight', 'Front Right Fender': 'front_right_fender', 'Front Left Fender': 'front_left_fender', 'Single Exhaust': 'single_exhaust', 'Front Right Door': 'front_right_door', 'Front Left Door': 'front_left_door', 'Rear Right Door': 'rear_right_door', 'Rear Left Door': 'rear_left_door'}
 
 class Car:
-    def __init__(self, id, vehicle):
-        self.name = "v" + id
-        self.vehicle = vehicle
-        self.damage = {}
+	def __init__(self, id, vehicle):
+		self.name = "v" + str(id)
+		self.vehicle = vehicle
+		self.damage = {}
+		self.description = ''
 
-    def set_damage(self, damage):
-        self.damage = damage
+	def set_damage(self, damage):
+		self.damage = damage
+
+	def set_description(self):
+		# Sample damage data from Damage Sensor
+		# test_dict = {
+		# 	'etk800_body_wagon': {
+		# 		'name': 'Wagon Unibody', 'damage': 0.0004748338081671415
+		# 	}, 
+		# 	'etk800_bumper_F': {
+		# 		'name': 'Front Bumper', 'damage': 0.15593220338983052
+		# 	}
+		# }
+		tmp_description = self.name
+		for key, val in self.damage.items():
+			position = CART_PARTS_DICT[self.damage[key]['name']]
+			value = self.damage[key]['damage']
+			tmp_description += ":" + position + "-" + str(value)
+		self.description = tmp_description
--- demo/BeamNGpy/scenario.py
+++ demo/BeamNGpy/scenario.py
@@ -2,6 +2,7 @@ import os
 import sys
 from beamngpy import BeamNGpy, Scenario, Vehicle, setup_logging
 from beamngpy.sensors import Damage
+from BeamNGpy.car import Car
 
 class BeamNg:
 	def __init__(self, bng_log, scenario):
@@ -9,20 +10,18 @@ class BeamNg:
 		self.bng_research = os.getenv('BNG_RESEARCH')
 		self.host = '127.0.0.1'
 		self.port = 64257
-		self.vehicles = []
+		self.cars = []
 		self.bng_log = bng_log
 		self.scenario = scenario
 		self.level = 'smallgrid'
 		self.timeout = 20
+		self.isCrash = False
 
-	def create_vehicle(self, vehicle_objs):
-		for v in vehicle_objs:
-			# Creates vehicle with associated id
-			vehicle = Vehicle(str(v.id))
-			damage = Damage()
-			# Attach Sensor
-			vehicle.attach_sensor('damage', damage)
-			self.vehicles.append(vehicle)
+	def create_vehicle(self, vehicles):
+		for i in range(len(vehicles)):
+			vehicle = Vehicle(str(vehicles[i].id))
+			vehicle.attach_sensor('damage', Damage())
+			self.cars.append(Car(i+1, vehicle)) # Vehicle name is v1, v2,...
 
 	def write_log(self, damage):
 		with open(self.bng_log, 'w') as f:
@@ -30,7 +29,7 @@ class BeamNg:
 	
 	def close_scenario(self, bng):
 		bng.kill_beamng()
-		sys.exit('----- Close BeamNG connection -----')
+		return
 	
 	def start_beamng(self):
 		# Instantiates a BeamNGpy instance
@@ -54,7 +53,8 @@ class BeamNg:
 		self.create_vehicle(bng.find_objects_class(vehicle_bng))
 
 		# bng.set_steps_per_second(10)
-		for vehicle in self.vehicles:
+		for car in self.cars:
+			vehicle = car.vehicle
 			bng.connect_vehicle(vehicle)
 			assert vehicle.skt
 
@@ -63,25 +63,28 @@ class BeamNg:
 		bng.step(waiting_steps, wait=True)
 		bng.start_scenario() # Start scenario
 
+		accident_log = {}
 		for _ in range(self.timeout):
-			# Collects sensor data every 30 steps
-			bng.step(30)
-
-			accident_log = {}
-			for vehicle in self.vehicles:
-				sensor = bng.poll_sensors(vehicle)['damage']
-				if (sensor['damage'] != 0): # Crash detected
-					print("Crash detected!")
-					accident_log.update( { vehicle.vid: sensor['partDamage'] } )
-			
 			empty = not bool(accident_log)
-			if not empty:
+			if empty:
+				# Collects sensor data every 30 steps
+				bng.step(30)
+				for car in self.cars:
+					vehicle = car.vehicle
+					sensor = bng.poll_sensors(vehicle)['damage']
+					if (sensor['damage'] != 0): # Crash detected
+						print("Crash detected!")
+						# self.isCrash = True
+						accident_log.update( { vehicle.vid: sensor['partDamage'] } )
+						car.damage =  sensor['partDamage']
+			else:
+				self.write_log(accident_log) # Write log file
 				waiting_steps = 175
-				self.write_log(accident_log)
 				bng.step(waiting_steps, wait=True)
 				print("Within time!")
-				self.close_scenario(bng)
+				break
 
 		# Timeout
-		print("Timed out!")
+		if not self.isCrash:
+			print("Timed out!")
 		self.close_scenario(bng)
