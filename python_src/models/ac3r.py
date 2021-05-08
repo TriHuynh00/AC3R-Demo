eca16edc633c5a4d88de7aaf0f466cf1719672bb
Harvey <me@ngvuong.com>
2021-05-08 14:26:34 +0200

    ISS48 - plot the trajectories between AC3R and AC3RPlus

---
+++ python_src/models/ac3r.py
@@ -0,0 +1,38 @@
+class Vehicle:
+    @staticmethod
+    def from_dict(vehicle_dict):
+        trajectory_points = []
+        for driving_action_dict in vehicle_dict["driving_actions"]:
+            # Iterate all the trajectory_list, i.e., list of poitns that define the segments
+            for trajectory_list in driving_action_dict["trajectory"]:
+                if len(trajectory_list) < 4:
+                    for t in trajectory_list:
+                        point = (t[0], t[1])
+                        if point not in trajectory_points:
+                            trajectory_points.append(point)
+                else:
+                    raise Exception("Too many points in the trajectory_dict")
+
+        return Vehicle(vehicle_dict["name"], trajectory_points)
+
+    def __init__(self, name, trajectory_points):
+        self.name = name
+        self.trajectory_points = trajectory_points
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+
+class CrashScenario:
+    @staticmethod
+    def from_json(ac3r_json_data):
+        vehicles = []
+        for vehicle_dict in ac3r_json_data["vehicles"]:
+            vehicles.append(Vehicle.from_dict(vehicle_dict))
+        return CrashScenario(vehicles)
+
+    def __init__(self, vehicles):
+        self.vehicles = vehicles
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
--- python_src/tests/test_trajectory.py
+++ python_src/tests/test_trajectory.py
@@ -2,26 +2,56 @@ import json
 import time
 import unittest
 
+import libs
+from models import ac3r
 from ac3r_plus import CrashScenario
 from simulation import Simulation
-from libs import _collect_sim_data, _collect_police_report
+from libs import _collect_sim_data, _calculate_score_police_report
+from matplotlib import pyplot as plt
 
 
 class TrajectoryTest(unittest.TestCase):
+    def test_json_trajectory(self):
+        sources = [libs.PATH_TEST + "/data/Case0_data.json",
+                   libs.PATH_TEST + "/data/Case1_data.json",
+                   libs.PATH_TEST + "/data/Case2_data.json",
+                   libs.PATH_TEST + "/data/Case3_data.json",
+                   libs.PATH_TEST + "/data/Case4_data.json",
+                   libs.PATH_TEST + "/data/Case5_data.json",
+                   libs.PATH_TEST + "/data/Case6_data.json"]
+        for source in sources:
+            with open(source) as file:
+                data = json.load(file)
+            ac3r_scenario = ac3r.CrashScenario.from_json(data)
+            colors = ["#ffdab9", "#b1c3de"]
+            for i, vehicle in enumerate(ac3r_scenario.vehicles):
+                trajectory_points = vehicle.trajectory_points
+                xs = [p[0] for p in trajectory_points]
+                ys = [p[1] for p in trajectory_points]
+                plt.plot(xs, ys, 'o-', label='O' + vehicle.name, color=colors[i])
+
+            with open(source) as file:
+                scenario_data = json.load(file)
+            crash_scenario = CrashScenario.from_json(scenario_data)
+            bng_vehicles = _collect_sim_data(crash_scenario)[2]
+            colors = ["#ff8c00", "#4069e1"]
+            for i, v in enumerate(bng_vehicles):
+                trajectory_points = v.road_pf.points
+                xs = [p[0] for p in trajectory_points]
+                ys = [p[1] for p in trajectory_points]
+                plt.plot(xs, ys, '*-', label='N' + v.vehicle.vid, color=colors[i])
+            plt.legend()
+            plt.show()
+
     def test_generate_trajectory(self):
-        sources = ["./data/Case6.json"]
+        sources = [libs.PATH_TEST + "/data/Case6_data.json"]
+
         for s in sources:
             with open(s) as file:
                 scenario_data = json.load(file)
             crash_scenario = CrashScenario.from_json(scenario_data)
-            timeout = time.time() + 60*1 # 2mins
+            timeout = time.time() + 90 * 1
             crash_scenario, bng_roads, bng_vehicles = _collect_sim_data(crash_scenario)
-            simulation = Simulation(bng_roads, bng_vehicles)
-            simulation.execute_scenario(timeout)
-            crash_scenario.sim_report = simulation.get_report()
 
-            report_data = _collect_police_report("./data/sample_report.json")
-            crash_scenario.cal_fitness(report_data)  # Calculate fitness score
 
-            print("Scores: ", crash_scenario.score)
         self.assertEqual(0, 0)
