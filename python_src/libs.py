76f17e5a4762359d3ca4813e5e93a0fd164b17ea
Harvey <me@ngvuong.com>
2021-05-08 15:05:56 +0200

    ISS48 - update lib file

--- python_src/libs.py
+++ python_src/libs.py
@@ -1,7 +1,35 @@
 import json
+import os
+from pathlib import Path
+
 from beamngpy import Road, Vehicle
 from beamngpy.sensors import Damage
+
+from ac3r_plus import CrashScenario
 from models import RoadProfiler, BNGVehicle
+from typing import List
+import scipy.stats as stats
+from bisect import bisect_left
+
+ROOT: Path = Path(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+PATH_TEST = str(ROOT.joinpath("tests"))
+
+
+def _key_exist_in_list(target, lis):
+    for d in lis:
+        if d["name"] == target:
+            return True
+    return False
+
+
+def _calculate_score_police_report(path):
+    with open(path) as file:
+        report_data = json.load(file)
+    score = 1
+    for v_id in report_data:
+        v_parts = report_data[v_id]
+        score += len(v_parts)
+    return score
 
 
 def _collect_police_report(path):
@@ -11,7 +39,7 @@ def _collect_police_report(path):
 
 
 # Collect simulation data from CrashScenario object
-def _collect_sim_data(crash_scenario):
+def _collect_sim_data(crash_scenario: CrashScenario):
     # JSON READ: Building scenario's streets
     bng_roads = []
     for road in crash_scenario.roads:
@@ -23,7 +51,7 @@ def _collect_sim_data(crash_scenario):
     for vehicle in crash_scenario.vehicles:
         trajectory = vehicle.generate_trajectory()
         initial_position = (trajectory[0][0], trajectory[0][1], 0)
-        v = Vehicle("scenario_player_" + str(vehicle.name),
+        v = Vehicle(str(vehicle.name),
                     model="etk800", licence=vehicle.name, color=vehicle.color)
         v.attach_sensor('damage', Damage())
         road_pf = RoadProfiler()
@@ -31,3 +59,27 @@ def _collect_sim_data(crash_scenario):
         bng_vehicles.append(BNGVehicle(v, initial_position, None, vehicle.rot_quat, road_pf))
 
     return crash_scenario, bng_roads, bng_vehicles
+
+
+def _VD_A(treatment: List[float], control: List[float]):
+    m = len(treatment)
+    n = len(control)
+
+    if m != n:
+        raise ValueError("Data d and f must have the same length")
+
+    r = stats.rankdata(treatment + control)
+    r1 = sum(r[0:m])
+
+    # Compute the measure
+    # A = (r1/m - (m+1)/2)/n # formula (14) in Vargha and Delaney, 2000
+    A = (2 * r1 - m * (m + 1)) / (2 * n * m)  # equivalent formula to avoid accuracy errors
+
+    levels = [0.147, 0.33, 0.474]  # effect sizes from Hess and Kromrey, 2004
+    # magnitude = ["negligible", "small", "medium", "large"]
+    # scaled_A = (A - 0.5) * 2
+
+    # magnitude = magnitude[bisect_left(levels, abs(scaled_A))]
+    estimate = A
+
+    return estimate
