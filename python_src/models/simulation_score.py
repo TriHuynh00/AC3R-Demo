d06399f221405bb7d76be5259a38c571816639e8
Harvey <me@ngvuong.com>
2021-05-17 21:49:52 +0200

    ISS51 - add score in case NO_CRASH

--- python_src/models/simulation_score.py
+++ python_src/models/simulation_score.py
@@ -1,8 +1,10 @@
 import models
-from typing import Tuple
+from typing import Tuple, List
+from shapely.geometry import Point
 
 MATCHING_CRASH_INDEX = 0
 MATCHING_NONCRASH_INDEX = 1
+NO_CRASH = 0
 
 
 class SimulationScore:
@@ -24,11 +26,27 @@ class SimulationScore:
     def formula(alpha: float, beta: float, tpl: Tuple[int, int, int] = (0, 0, 0)):
         return 1 + (alpha * tpl[MATCHING_CRASH_INDEX]) + (beta * tpl[MATCHING_NONCRASH_INDEX])
 
+    @staticmethod
+    def distance_between_two_players(players: List[models.Player]):
+        p0 = Point(players[0].positions[-1][0], players[0].positions[-1][1])
+        p1 = Point(players[1].positions[-1][0], players[1].positions[-1][1])
+        return round(-p0.distance(p1), 2)
+
     def calculate(self, debug: bool = False):
+        # If a crash doesn't occur, a score is distance between 2 vehicles' position
+        if self.simulation.status is NO_CRASH:
+            if debug is True:
+                print("Log SimulationScore.calculate() - NO_CRASH: ")
+                for player in self.simulation.players:
+                    print(player.vehicle.vid)
+                    print(player.positions)
+            return self.distance_between_two_players(self.simulation.players)
+
+        # Else
         data_targets = self.simulation.targets
         data_outputs = self.simulation.get_data_outputs()
         if debug is True:
-            print("Log SimulationScore.calculate(): ")
+            print("Log SimulationScore.calculate() - CRASH: ")
             print(data_targets)
             print(data_outputs)
 
