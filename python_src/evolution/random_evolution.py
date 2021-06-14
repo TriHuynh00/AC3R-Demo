46fab1cef6f73d3fd6e3308af7e536fb262747a8
Harvey <me@ngvuong.com>
2021-06-14 10:54:51 +0200

    ISS32 - add visualization and write to logfile

--- python_src/evolution/random_evolution.py
+++ python_src/evolution/random_evolution.py
@@ -7,7 +7,7 @@ FIRST = 0
 
 
 class RandomEvolution:
-    def __init__(self, scenario, fitness, generate, generate_params, select, epochs=1, fitness_repetitions=1,
+    def __init__(self, scenario, fitness, generate, generate_params, select, logfile, epochs=1, fitness_repetitions=1,
                  select_aggregate=None):
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
         creator.create("Individual", list, fitness=creator.FitnessMax)
@@ -34,6 +34,7 @@ class RandomEvolution:
         self.epochs = epochs
         self.orig_ind = scenario
         self.fitness_repetitions = fitness_repetitions
+        self.logfile = logfile
 
     def run(self):
         pop = self.toolbox.population(n=1)
@@ -72,10 +73,10 @@ class RandomEvolution:
             s2: CrashScenario = pop[0][0]
             print(f'Last Ind: '
                   f'(Speed v1-{s1.vehicles[0].get_speed()}) '
-                  f'(Speed v2-{s1.vehicles[0].get_speed()}) '
+                  f'(Speed v2-{s1.vehicles[1].get_speed()}) '
                   f'(Fitness Value-{best_ind.fitness.values[0]})')
             print(f'New Ind: '
-                  f'(Speed v1-{s2.vehicles[1].get_speed()}) '
+                  f'(Speed v1-{s2.vehicles[0].get_speed()}) '
                   f'(Speed v2-{s2.vehicles[1].get_speed()}) '
                   f'(Fitness Value-{pop[0].fitness.values[0]})')
             ##############################################################################
@@ -90,11 +91,16 @@ class RandomEvolution:
             print("We select the best scenario: ")
             s: CrashScenario = best_ind[0]
             print(f'Best Ind: '
-                  f'(Speed v1-{s.vehicles[1].get_speed()}) '
+                  f'(Speed v1-{s.vehicles[0].get_speed()}) '
                   f'(Speed v2-{s.vehicles[1].get_speed()}) '
                   f'(Fitness Value-{best_ind.fitness.values[0]})')
             print("-----------------------------------------------------------------------------------------------")
             ##############################################################################
+
+            # Write to logfile
+            self.logfile.write(f'{s.vehicles[0].get_speed()},'
+                               f'{s.vehicles[1].get_speed()},'
+                               f'{best_ind.fitness.values[0]}\n')
 
         print("Evolution time: ", time.time() - start_time)
         print("End of evolution")
--- python_src/experiment.py
+++ python_src/experiment.py
@@ -1,6 +1,10 @@
 import json
 import numpy
 import libs
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import time
 
 from evolution import LogBook, RandomEvolution, OpoEvolution, Mutator, Fitness, Generator, Selector
 from models import SimulationFactory, Simulation, SimulationScore
@@ -8,8 +12,10 @@ from models.ac3rp import CrashScenario
 
 
 class Experiment:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, simulation_name: str = None):
         try:
+            tmp_simulation_name = 'beamng_executor/sim_$(id)'.replace('$(id)', time.strftime('%Y-%m-%d--%H-%M-%S', time.localtime()))
+            self.simulation_name = tmp_simulation_name if simulation_name is None else simulation_name
             with open(file_path) as file:
                 self.scenario = json.load(file)
         except Exception as ex:
@@ -21,12 +27,16 @@ class Experiment:
             return False
 
         # Seeds
-        numpy.random.seed(64)
+        # numpy.random.seed(64)
         # Calculate the expected score
         sim_factory: SimulationFactory = SimulationFactory(CrashScenario.from_json(self.scenario))
         simulation: Simulation = Simulation(sim_factory=sim_factory)
         expected_score: float = SimulationScore(simulation).get_expected_score()
         logbook: LogBook = LogBook(score=expected_score)
+
+        # Write data file
+        rev_logfile = open("data/random/" + self.simulation_name + ".csv", "a")
+        rev_logfile.write("v1,v2,score\n")
 
         # Experiment run
         rev = RandomEvolution(
@@ -36,8 +46,9 @@ class Experiment:
             generate=Generator.generate_random_from,
             generate_params={"min": 10, "max": 50},
             select=Selector.by_fitness_value,
-            # select_aggregate=libs._VD_A,
-            epochs=30
+            # select_aggregate=numpy.mean,
+            epochs=3,
+            logfile=rev_logfile
         )
         oev = OpoEvolution(
             scenario=CrashScenario.from_json(self.scenario),
@@ -52,7 +63,40 @@ class Experiment:
             epochs=30
         )
         rev.run()
-        oev.run()
+        # oev.run()
+
+        # Close logfiles
+        rev_logfile.close()
 
         # Provide plot chart
-        logbook.visualize(rev.logbook, oev.logbook, "Random", "OPO")
+        # logbook.visualize(rev.logbook, oev.logbook, "Random", "OPO")
+
+    def visualize(self):
+        root_path = "data/rov/"
+        case_paths = ["c0/", "c1/", "c2/", "c3/", "c4/", "c5/", "c6/"]
+        # case_paths = ["c6/"]
+        for case in case_paths:
+            df = pd.DataFrame()
+            case_path = root_path + case
+            for file in ["a.csv", "b.csv", "c.csv", "d.csv", "e.csv"]:
+                data = pd.read_csv(case_path + file)
+                a = pd.DataFrame({'group': numpy.repeat(file[0].upper(), 30), 'value': data["score"].to_numpy()})
+                df = df.append(a)
+
+            plt.figure(figsize=(8, 6))
+            # boxplot
+            ax = sns.boxplot(x='group', y='value', data=df)
+            # add stripplot
+            ax = sns.stripplot(x='group', y='value', data=df, color="orange", jitter=0.2, size=5)
+            # add maximum
+            plt.plot(numpy.repeat(1.7, 5), label="Maximum", linewidth=1, color='#66af78')
+            plt.plot(numpy.repeat(1.2, 5), label="Original", linewidth=1, color='#c6595d')
+
+            # add title
+            plt.title(f'Boxplot of Case {case[1]} by Different Repetition')
+            plt.xlabel('Repetition')
+            plt.ylabel('Fitness Score')
+
+            # show the graph
+            plt.legend()
+            plt.show()
