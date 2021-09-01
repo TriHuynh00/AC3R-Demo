de9d497d2f98c79548adc552aa025375cfa9a877
Harvey <me@ngvuong.com>
2021-09-01 13:10:04 +0200

    Minor update'

--- python_src/main.py
+++ python_src/main.py
@@ -48,65 +48,50 @@ def evol_scenario(scenario_file):
     experiment.run()
 
 
+def visualize_csc(fn):
+    with open(fn+"_c.json") as file:
+        csc = json.load(file)
+
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+
+    for color in ["red", "blue"]:
+        csc_cp = csc["vehicles"][color]["crash_point"]["coordinates"]
+        v1_line = csc["vehicles"][color]["trajectories"]["simulation_trajectory"]
+        aspect_ratio = csc["vehicles"][color]["dimensions"]["car_length_sim"] / csc["vehicles"][color]["dimensions"][
+            "car_length"]
+        csc_cp = [csc_cp[0] * aspect_ratio, csc_cp[1] * aspect_ratio]
+
+        # assign coordinates
+        dist_x = - csc_cp[0]
+        dist_y = - csc_cp[1]
+        xs = [p[0] + dist_x for p in v1_line]
+        ys = [p[1] + dist_y for p in v1_line]
+        plt.plot(xs, ys, '-', label=f'Vehicle {color}', color=color)
+    # plt.plot(0, 0, 'o-', color="black")
+    plt.legend()
+    plt.gca().set_aspect("equal")
+    plt.xlim([-100, 100])
+    plt.ylim([-100, 100])
+    plt.title(f'CRISCEs crash_report_{fn.split("/")[1]}')
+    plt.show()
+    fig.savefig(f'crisce_{fn.split("/")[1]}.png')
+
+
 # make sure we invoke cli
 if __name__ == '__main__':
-    # cli()
+    cli()
+    # scenario_files = ["data/crisce/crash_report_122080_data.json"]
+    # for s in scenario_files:
+    #     with open(s) as file:
+    #         scenario_data = json.load(file)
+    #     VehicleTrajectoryVisualizer.plot_ac3r(s)
+    #     VehicleTrajectoryVisualizer.plot_ac3rp(s)
 
-    # for s in ["data/Case4_data.json"]:
-    #     for i in np.arange(start=1, stop=6, step=1):
-    #         sim_name: str = s[5:11] + str(i)
-    #         print(f'Level {sim_name}...')
-    #         exp: Experiment = Experiment(file_path=s, simulation_name=sim_name)
-    #         exp.run(method_name="Random")
-    #     print(f'-------------------- End of {s} --------------------------------------------------------------')
-    #     print()
+    # scenario_files = ["data/122080"]
+    # for f in scenario_files:
+    #     VehicleTrajectoryVisualizer.plot_ac3r(f+"_a.json")
+        # visualize_csc(f)
 
-    scenario_files = ["data/crisce/crash_report_100343_data.json",
-                      "data/crisce/crash_report_103378_data.json",
-                      "data/crisce/crash_report_105222_data.json",
-                      "data/crisce/crash_report_108812_data.json",
-                      "data/crisce/crash_report_108909_data.json",
-                      "data/crisce/crash_report_119897_data.json",
-                      "data/crisce/crash_report_120013_data.json",
-                      "data/crisce/crash_report_120278_data.json",
-                      "data/crisce/crash_report_120305_data.json",
-                      "data/crisce/crash_report_120565_data.json",
-                      "data/crisce/crash_report_122080_data.json",
-                      "data/crisce/crash_report_122168_data.json",
-                      "data/crisce/crash_report_128697_data.json"]
-    for s in scenario_files:
-        with open(s) as file:
-            scenario_data = json.load(file)
-        sim_factory = SimulationFactory(CrashScenario.from_json(scenario_data))
-        simulation = Simulation(sim_factory=sim_factory, file_name=s)
-        simulation.execute_scenario(timeout=30)
-        # print(f'Simulation Score: {SimulationScore(simulation).calculate()} / '
-        #       f'{SimulationScore(simulation).get_expected_score()}')
 
-    # import libs
-    # import scipy.stats as stats
-    # import numpy as np
 
-    # f1, f2 = np.array([0, 7, 17, 2, 9, 0]), np.array([0, 1, 1, 1, 3, 0])
-    # f1c, f2c = np.array([1.4, 1.8, 1.4, 1.6, -16, 1.7]), np.array([1.4, 1.8, 1.4, 1.6, -12.86, 1.7])
-
-    # print(f'Average Time:  Random {str(round(f1.mean(), 2))}; OpO {str(round(f2.mean(), 2))}')
-    # print(f'Average Score: Random {str(round(f1c.mean(), 2))}; OpO {str(round(f2c.mean(), 2))}')
-
-    # # Calculate the p-value to know if two populations are different
-    # alpha = 0.05  # significance level to reject H0
-    # stat, p = stats.mannwhitneyu(f1, f2)
-    # print('Test Statistics=%.3f, p=%.3f' % (stat, p))
-    # if p > alpha:
-    #     print('H0: The two populations are equal versus.')
-    # else:
-    #     print('H1: The two populations are different.')
-    #     print("Compare: ")
-    #     print(f'Random: {f1}')
-    #     print(f'OpO: {f2}')
-    #     # To understand how different they were (the effect size)
-    #     # we used VDA
-    #     if libs._VD_A(f1, f2) > 0.5:
-    #         print(f'Random faster: {f1}')
-    #     else:
-    #         print(f'OpO faster: {f2}')
