bda1b78d369bbb5a6d90511a4a983120bc30e765
Harvey <me@ngvuong.com>
2021-10-03 19:04:03 +0200

    ISS78 - add support functions

--- python_src/models/ac3rp/common.py
+++ python_src/models/ac3rp/common.py
@@ -4,6 +4,9 @@ from numpy.ma import arange
 from shapely.geometry import LineString, Point
 from shapely.affinity import translate, rotate
 from math import degrees, atan2, copysign
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+from typing import List, Dict, Tuple
 
 # Constants
 rounding_precision = 3
@@ -204,3 +207,188 @@ def interpolate(road_nodes, sampling_unit=interpolation_distance):
     else:
         return list(zip([round(v, rounding_precision) for v in new_x_vals],
                         [round(v, rounding_precision) for v in new_y_vals]))
+
+
+def is_parallel(line1: List, line2: List):
+    """
+    Take 2 list of coordinates and check if they are parallel.
+    """
+    ax = line1[1][0] - line1[0][0]
+    ay = line1[1][1] - line1[0][1]
+    bx = line2[1][0] - line2[0][0]
+    by = line2[1][1] - line2[0][1]
+
+    if ax * by == ay * bx:
+        return True
+    return False
+
+
+def is_inside_polygon(point: Point, polygon: Polygon):
+    """
+    Check if the point belongs to the given polygon.
+    """
+    return polygon.contains(point)
+
+
+def generate_random_point_within_circle(center: Point, minR: int, maxR: int):
+    """
+    Generate a random point whose radius is less than maxR
+    and larger than minR.
+
+    Returns:
+        Tuple (x, y)
+    """
+    import math
+    import random
+    r = math.sqrt(random.random() * (maxR ** 2 - minR ** 2) + minR ** 2)
+    theta = random.random() * 2 * math.pi
+    x = center.x + r * math.cos(theta)
+    y = center.y + r * math.sin(theta)
+    return x, y
+
+
+def generate_random_point_within_line(center: Point, minR: int, maxR: int, delta: Tuple):
+    """
+    Generate a random point by given a line equation,
+    central point, min and max distance.
+
+    Args:
+        center (Point): Given a center point
+        minR, maxR (int): Minimum and maximum distance between a new point and center point
+        delta (Tuple): Given line equation (a, c) e.g y = ax + b
+
+    Returns:
+        Tuple (x, y)
+    """
+    import math
+    import random
+    # Reference:
+    # https://math.stackexchange.com/questions/426807/how-does-this-vector-addition-work-in-geometry
+    random_direction = 1 if random.random() < 0.5 else -1
+
+    if delta[0] is None:  # any x, y unchanged
+        point2 = Point(center.x + random.random(), delta[1])
+    elif delta[1] is None:  # any y, x unchanged
+        point2 = Point(delta[0], center.y + random.random())
+    else:
+        a, b = delta
+        x = center.x + random.random()
+        point2 = Point(x, a * x + b)
+
+    distance = random.randint(minR, maxR)
+    v = (point2.x - center.x, point2.y - center.y)
+    length_v = math.sqrt(v[0] ** 2 + v[1] ** 2)
+    x_new = center.x + distance / length_v * v[0] * random_direction
+    y_new = center.y + distance / length_v * v[1] * random_direction
+
+    return x_new, y_new
+
+
+def translate_ls_to_new_origin(lst: LineString, new_origin: Tuple):
+    """
+    Translate an existed linestring to a new origin.
+
+    Args:
+        lst (LineString): An existed linestring
+        new_origin (Tuple): A new origin of a new linestring eg (x, y)
+    Returns:
+        new_lst (LineString)
+    """
+    import math
+    first, last = lst.boundary
+    dx = first.x - new_origin[0]
+    dy = first.y - new_origin[1]
+
+    new_lst = list()
+    for p in list(lst.coords):
+        new_lst.append((p[0] - dx, p[1] - dy))
+
+    if math.isclose(lst.length, LineString(new_lst).length, rel_tol=1e-3) is False:
+        # two values are approximately equal or “close” to each other, 3 digits after comma
+        raise Exception("Generated new line is not the same length!")
+    if not is_parallel(lst.coords, new_lst):
+        raise Exception("Generated new line is not parallel to the old line!")
+    return LineString(new_lst)
+
+
+def mutate_initial_point(plt, vehicle_lst: LineString,
+                         road_poly: Polygon,
+                         delta: Tuple,
+                         minR: int, maxR: int, num_points: int = 1):
+    """
+    Mutate an initial point of vehicle trajectory by generated a new initial point
+
+    Args:
+        vehicle_lst (LineString): LineString of all vehicle's segments
+        road_poly (Polygon): A road polygon that a vehicle stays in
+        delta (Tuple): Given line equation (a, c) y = ax + b going through the initial point of vehicle. This equation
+                       also guarantees the generated point is in the same line of the initial point
+        minR, maxR (int): Minimum and maximum distance between a new point and center point
+        num_points (int): An expected number of point we would generate
+
+
+    Returns:
+        random_points (List): A list of new initial points on the circle or on the same line of an old initial point
+    """
+    print(num_points)
+    random_points = list()
+    first, last = vehicle_lst.boundary
+    for i in range(num_points):
+        #         (x, y) = generate_random_point_within_circle(first, minR, maxR)
+        (x, y) = generate_random_point_within_line(first, minR, maxR, delta)
+        if not is_inside_polygon(Point(x, y), road_poly):
+            plt.plot(x, y, '.', color="red")
+            pass
+        else:
+            plt.plot(x, y, '.', color="green")
+            random_points.append((x, y))
+    return random_points
+
+
+def cal_equation_line_two_points(p1, p2):
+    """
+    Compute a line equation going through 2 random points.
+
+    Returns:
+        Tuple (a, b): y = ax + b
+    """
+    if p1.x == p2.x:
+        # print("Line Solution is x = {x0}".format(x0=p1.x))
+        return p1.x, None
+    if p1.y == p2.y:
+        # print("Line Solution is y = {y0}".format(y0=p1.y))
+        return None, p1.y
+
+    a = (p1.y - p2.y) / (p1.x - p2.x)
+    b = (p1.x * p2.y - p2.x * p1.y) / (p1.x - p2.x)
+    # print("Line Solution is y = {a}x + {b}".format(a=a, b=b))
+    return a, b
+
+
+def cal_equation_line_one_point_and_line(p1: Point, d2: tuple):
+    """
+    Compute a line equation going through 1 point and making a right angle with
+    another line equation.
+
+    Returns:
+        Tuple (a, b): y = ax + b
+    """
+    x1, y1 = p1.x, p1.y
+
+    # d1: y1 = a1*x1 + b1
+    # d2: y2 = a2*x2 + b2
+
+    # d1 right angle d2 => a1 * a2 = -1
+    a2, b2 = d2
+    if a2 == 0:
+        # print("Line Solution is y = {y0}".format(y0=y1))
+        return None, y1
+    if a2 is None:
+        # print("Line Solution is x = {x0}".format(x0=x1))
+        return x1, None
+    a1 = -1 / a2
+
+    # p1 in d1
+    b1 = y1 - (a1 * x1)
+    # print("Line Solution is y = {a}x + {b}".format(a=a1, b=b1))
+    return a1, b1
